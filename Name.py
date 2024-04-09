95f584db795622bbdd5c4e8f023531a0760add27
Thomas Mpherwane <Thomas.Mpherwane8@gmail.com>
2024-04-09 12:26:48 +0200

    Thomas

--- Name.py
+++ Name.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 def display_name(name):
-    print(f"Your name is {name}")
+    print(f"Your name is {abbreviated_name}")
 
 
 
-name = input("Please enter your name: ")    
+name = input("Please enter your name: ") 
+
+abbreviated_name = name[:3] 
 display_name(name)
